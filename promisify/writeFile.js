f54bc554b09e76d2c0de7d834b13ed1ad24ed4f4
Baraka Mahili <georgesmahili@gmail.com>
2019-05-21 23:41:17 -0400

    removed test file

--- promisify/writeFile.js
+++ promisify/writeFile.js
@@ -1,6 +1,6 @@
 // Bring in the file system module
 const fs = require('fs');
-// Bring in the promisify from the util object
+// Bring in promisify from the util object
 const { promisify } = require('util');
 
 // Create a custom callback function to write a file
@@ -12,9 +12,9 @@ writeFile('sample.txt', 'This is some new text to test the promisify')
 
 
 const appendFile = promisify(fs.appendFile)// turns the fs.appendFile callback function into a promise
-appendFile('sample33.txt', ', Add this to the other file')
+appendFile('sample.txt', ', Add this to the other file')
     .then(() => console.log('Done'))
     .catch(e=>logError(e));
 
 // Helper Function:
-const logError = error => console.log('Error creating file', error);
+const logError = error => console.log('Error processing file', error);
