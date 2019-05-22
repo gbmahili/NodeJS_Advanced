96f0bb022ba85887c0c18c1928d90c3ba286461a
Baraka Mahili <georgesmahili@gmail.com>
2019-05-21 23:25:09 -0400

    Added appendfile

--- promisify/writeFile.js
+++ promisify/writeFile.js
@@ -4,8 +4,13 @@ const fs = require('fs');
 const { promisify } = require('util');
 // Create a custom callback function to write a file
 const writeFile = promisify(fs.writeFile);
-
+const logError = error => console.log('Error creating file', error)
 //Use the new promiseFunction
 writeFile('sample.txt', 'This is some new text to test the promisify')
     .then(() => console.log('Fille written successfully'))
-    .catch(error => console.log('Error creating file', error));
+    .catch(e=>logError(e));
+
+const appendFile = promisify(fs.appendFile)
+appendFile('sample.txt', ', Add this to the other file')
+    .then(() => console.log('Done'))
+    .catch(e=>logError(e));
