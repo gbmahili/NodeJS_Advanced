153a5604bb066e4014ead80d610d1834a97e213e
Baraka Mahili <georgesmahili@gmail.com>
2019-05-22 00:01:15 -0400

    Made a pyramid of doom

---
+++ promisify/beepMe.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { promisify } = require('util');
+const writeFile = promisify(fs.writeFile);
+const unlink = promisify(fs.unlink);
+
+const beep = () => console.log('\u0007');// Makes your Mac to beep...really...it does
+
+const delay = seconds => new Promise(resolves => {
+    setTimeout(resolves, seconds * 1000);
+});
+
+const doStuffSequentially = () => Promise.resolve()
+    .then(() => console.log('Starting...'))
+    .then(() => delay(1))
+    .then(() => 'Waiting...')
+    .then(console.log)
+    .then(() => delay(2))
+    .then(() => writeFile('file.txt', 'Testing more complex promises'))
+    .then(beep)
+    .then(() => 'file.txt created.')//this will return the text 'file.txt created'
+    .then(console.log)//text returned above will be logged here
+    .then(() => delay(3))
+    .then(() => unlink('file.txt'))
+    .then(beep)
+    .then(()=>'file.txt removed.')
+    .then(console.log)
+    .catch(console.error)//will throw the first error to the console
+
+doStuffSequentially();
